6dba6a4cf4d4c94ea045da765d4861c2cb5bf043
SkyLouna <skylouna@laposte.net>
2019-11-06 16:22:32 +0100

    Add high scores and credits views

--- app/src/main/java/ch/raylouna/spacetml/MainActivity.java
+++ app/src/main/java/ch/raylouna/spacetml/MainActivity.java
@@ -5,6 +5,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.view.WindowManager;
 import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
@@ -14,6 +15,8 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+
         final Intent playIntent = new Intent(this, GameActivity.class);
         Button playButton = this.findViewById(R.id.btnPlayGame);
         playButton.setOnClickListener(new View.OnClickListener() {
@@ -21,6 +24,26 @@ public class MainActivity extends AppCompatActivity {
             public void onClick(View view) {
 
                 startActivity(playIntent);
+            }
+        });
+
+        final Intent highScoresIntent = new Intent(this, HighScoresActivity.class);
+        Button highScoresButton = this.findViewById(R.id.btnHighscore);
+        highScoresButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                startActivity(highScoresIntent);
+            }
+        });
+
+        final Intent creditsIntent = new Intent(this, CreditsActivity.class);
+        Button creditsButton = this.findViewById(R.id.btnCredits);
+        creditsButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                startActivity(creditsIntent);
             }
         });
     }
